87a1692312133c6370f485b8b8756bf8ca9f44d2
ArXamE <65113898+ArXamE@users.noreply.github.com>
2020-05-12 17:54:13 +0800

    Add files via upload

---
+++ quiz.php
@@ -0,0 +1,596 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Quiz</title>
+    <link href = "style.css" rel = "stylesheet">
+</head>
+<body>
+
+
+
+	<?php 
+		$_POST["htmlq"]
+	 ?>
+
+<div class="quiz1">
+	<center><u><b>HTML TAGS AND ATTRIBUTES QUIZ</b></u></center>
+	<br><br>
+	<br><br>
+    <b>Instructions:</b> Choose the best answer for the given question.
+
+<br>
+<br>
+<br>
+<br>
+
+	<ol>
+
+		<li><b> < tr > Tag means?" </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu1A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu1B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu1C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu1D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> < td > Tag means?" </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu2" value="B">
+		<label for='qu2A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu2" value="B">
+		<label for='qu2B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu2" value="B">
+		<label for='qu2C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu2" value="B">
+		<label for='qu2D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To create a table cell that spans several columns, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu3" value="B">
+		<label for='qu3A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu3" value="B">
+		<label for='qu3B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu3" value="B">
+		<label for='qu3C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu3" value="B">
+		<label for='qu3D'>D.  Nani </label>
+		</div>	
+
+
+<br><br>
+
+		<li><b> To create a table cell that spans several rows, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu4" value="B">
+		<label for='qu4A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu4" value="B">
+		<label for='qu4B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu4" value="B">
+		<label for='qu4C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu4" value="B">
+		<label for='qu4D'>D.  Nani </label>
+		</div>	
+
+
+<br><br>
+
+		<li><b> To create a row group consisting of header rows, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu5" value="B">
+		<label for='qu5A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu5" value="B">
+		<label for='qu5B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu5" value="B">
+		<label for='qu5C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu5" value="B">
+		<label for='qu5D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To create a row group consisting of footer rows, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu6" value="B">
+		<label for='qu6A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu6B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu10" value="B">
+		<label for='qu6C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu6" value="B">
+		<label for='qu6D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To create a row group consisting of rows used in the body of the table, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu7" value="B">
+		<label for='qu7A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu7" value="B">
+		<label for='qu7B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu7C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu7" value="B">
+		<label for='qu7D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To create a column group, what attribute must be used? " </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu7" value="B">
+		<label for='qu7A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu7B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='quC'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu1D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To define the padding within the table cells, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu8" value="B">
+		<label for='qu8A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu8B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu9C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu9D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To define the space between table cells, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu10" value="B">
+		<label for='qu10A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu10B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu10C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu10D'>D.  Nani </label>
+		</div>	
+
+		<div class="submit">
+		<input type="submit" name="Submit" action="quizresult.php">
+		</div>
+
+	</ol>
+
+	<div class="quiz2">
+	<center><u><b>GETTING STARTED WITH PHP QUIZ</b></u></center>
+	<br><br>
+	<br><br>
+    <b>Instructions:</b> Answer the following question. <b>NO ACRONYMS</b>
+
+<br>
+<br>
+<br>
+<br>
+
+	<ol>
+
+		<li><b> < tr > Tag means?" </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu1A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu1B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu1C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu1D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> < td > Tag means?" </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu2" value="B">
+		<label for='qu2A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu2" value="B">
+		<label for='qu2B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu2" value="B">
+		<label for='qu2C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu2" value="B">
+		<label for='qu2D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To create a table cell that spans several columns, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu3" value="B">
+		<label for='qu3A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu3" value="B">
+		<label for='qu3B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu3" value="B">
+		<label for='qu3C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu3" value="B">
+		<label for='qu3D'>D.  Nani </label>
+		</div>	
+
+
+<br><br>
+
+		<li><b> To create a table cell that spans several rows, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu4" value="B">
+		<label for='qu4A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu4" value="B">
+		<label for='qu4B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu4" value="B">
+		<label for='qu4C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu4" value="B">
+		<label for='qu4D'>D.  Nani </label>
+		</div>	
+
+
+<br><br>
+
+		<li><b> To create a row group consisting of header rows, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu5" value="B">
+		<label for='qu5A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu5" value="B">
+		<label for='qu5B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu5" value="B">
+		<label for='qu5C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu5" value="B">
+		<label for='qu5D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To create a row group consisting of footer rows, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu6" value="B">
+		<label for='qu6A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu6B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu10" value="B">
+		<label for='qu6C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu6" value="B">
+		<label for='qu6D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To create a row group consisting of rows used in the body of the table, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu7" value="B">
+		<label for='qu7A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu7" value="B">
+		<label for='qu7B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu7C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu7" value="B">
+		<label for='qu7D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To create a column group, what attribute must be used? " </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu7" value="B">
+		<label for='qu7A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu7B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='quC'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu1D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To define the padding within the table cells, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu8" value="B">
+		<label for='qu8A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu8B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu9C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu9D'>D.  Nani </label>
+		</div>	
+
+<br><br>
+
+		<li><b> To define the space between table cells, what attribute must be used? </b></li>
+		<br>
+
+		<div>
+		<input type="radio" name="qu10" value="B">
+		<label for='qu10A'>A.  Nani </label>
+		</div>
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu10B'>B.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu10C'>C.  Nani </label>
+		</div>
+
+
+		<div>
+		<input type="radio" name="qu1" value="B">
+		<label for='qu10D'>D.  Nani </label>
+		</div>	
+
+		<div class="submit">
+		<input type="submit" name="Submit" action="quizresult.php">
+		</div>
+
+	</ol>
+	
+
+</div>
+</body>
+</html>
